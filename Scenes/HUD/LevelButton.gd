3585ac29982a31782f748d4b828be6fc8ab35653
Radek Polczyk <dolorismachina@gmail.com>
2019-06-19 20:41:54 +0100

    Display data associated with a level on its button

--- Scenes/HUD/LevelButton.gd
+++ Scenes/HUD/LevelButton.gd
@@ -8,5 +8,10 @@ func _ready():
 	$Label.text = str(level_id + 1)
 
 
+func display_saved_data(data):
+	print(data.time)
+	$Score.text = str(data.score) + "pts"
+	$Time.text = str(data.time) + "s"
+
 func _on_LevelButton_pressed():
 	emit_signal("level_selected", level_id)
--- Scenes/HUD/LevelSelect.gd
+++ Scenes/HUD/LevelSelect.gd
@@ -8,6 +8,10 @@ func _ready():
 		if i.is_in_group("level_select_button"):
 			i.connect("level_selected", self, "on_level_selected")
 			
+			var data = get_parent().get_parent().get_node("SaveSystem").get_data(i.level_id)
+			i.display_saved_data(data)
+			print(data)
+			
 			
 func on_level_selected(id):
 	visible = false
